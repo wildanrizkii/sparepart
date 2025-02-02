0ed8de96f46c4fd4a7c491aea817e68db504ad4e
wildanrizkii <wildanrizki9560@gmail.com>
2025-02-02 16:01:11 +0700

    push

--- src/app/kelola/material/page.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import Material from "@/components/Kelola/Material/Material";
-import SideNavigation from "@/components/SideNavigation";
-
-export default function Dashboard() {
-  return <SideNavigation menu="2-3" submenu="2" konten={<Material />} />;
-}
